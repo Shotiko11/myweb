35a2edd63b96b2c270690c7ea4dc01573f8f25f6
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-08-30 17:03:41 +0400

    Contact information

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,25 @@
-import OnlyHeader from '@/app/OnlyHeader'
-import React from 'react'
+import React from 'react';
+import '../cssfiles/Contact.css'
+import OnlyHeader from '@/app/OnlyHeader';
 
-const Contact = () => {
+const Contact = ({ randomNumber }) => {
   return (
+    <div>
     <OnlyHeader />
-  )
-}
+    <div className="contact-container">
+      <h1>Contact Information</h1>
+      <p>For inquiries and assistance, please contact us:</p>
+      <div className="contact-details">
+        <p>Phone Number: +1 984 123 1245</p>
+      </div>
+    </div>
+    </div>
+  );
+};
 
-export default Contact
+
+
+
+
+export default Contact;
+
