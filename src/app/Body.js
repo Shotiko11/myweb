4f7282d473fd372e59f730e9b502b66ebd6acf10
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-08-31 15:23:06 +0400

    pictures has been changed

--- src/app/Body.js
+++ src/app/Body.js
@@ -1,51 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
-import '../cssfiles/body.css'; 
+import '../cssfiles/body.css';
 import { texts } from './LongTexts';
 
-
 const Body = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const images = [
+    '/images/imagefour.jpg',
+    '/images/whyus.jpg',
+    '/images/onlyinspired.png',
+  ];
+
+  const imageClickHandler = (index) => {
+    setSelectedImage(index);
+  };
+
   return (
     <div className="home-container">
       <div className="home-content">
-        <div className="image-text-container">
-          <div className="centered-image">
-            <Image  src="/images/imagefour.jpg" alt="Image One" width={300} height={200} />
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`image-text-container ${selectedImage === index ? 'selected' : ''}`}
+            onClick={() => imageClickHandler(index)}
+          >
+            <div className="centered-image">
+              <Image src={image} alt={`Image ${index}`} width={300} height={200} />
+            </div>
+            {selectedImage === index && (
+              <div className="text">
+                <h2>{index === 0 ? 'Welcome to the best tech company Website' : index === 1 ? 'Why you have to choose us?' : 'Shaping the Future with Innovation'}</h2>
+                <p>{index === 0 ? texts.welcome : index === 1 ? texts.whyUs : texts.WaitFromUs}</p>
+              </div>
+            )}
           </div>
-          <div className="text">
-            <h2>Welcome to the best tech company Website</h2>
-            <p>
-              {texts.welcome}
-            </p>
-          </div>
-        </div>
-
-        <div className="image-text-container">
-          <div className="text">
-            <h2>why you have to choose us?</h2>
-            <p>
-              {texts.whyUs}
-            </p>
-          </div>
-          <div className="centered-image">
-            <Image src="/images/whyus.jpg" alt="Image Two" width={300} height={300} />
-          </div>
-        </div>
-
-        <div className="image-text-container">
-          <div className="centered-image">
-            <Image src="/images/onlyinspired.png" alt="Image Three" width={300} height={200} />
-          </div>
-          <div className="text">
-            <h2>Shaping the Future with Innovation</h2>
-            <p>
-              {texts.WaitFromUs}
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default Body;
